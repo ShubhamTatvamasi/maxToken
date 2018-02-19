7276694675e2e9bed966eaa921ef8fb584958bfd
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-02-19 22:46:23 +0530

    Create maxToken.sol

---
+++ maxToken.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.20;
+
+contract maxToken {
+    // Public variables of the token
+    string public name = "Max Token";
+    string public symbol = "MXT";
+    uint8 public decimals = 18;
+    uint256 public totalSupply = 2**256 - 1;
+
+    /* This creates an array with all balances */
+    mapping (address => uint256) public balanceOf;
+
+    // This generates a public event on the blockchain that will notify clients
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /* Initializes contract with initial supply tokens to the creator of the contract */
+    function maxToken() public {
+        balanceOf[msg.sender] = totalSupply;              // Give the creator all initial tokens
+    }
+
+    /* Send coins */
+    function transfer(address _to, uint256 _value) public {
+        require(balanceOf[msg.sender] >= _value);           // Check if the sender has enough
+        require(balanceOf[_to] + _value > balanceOf[_to]);  // Check for overflows
+        balanceOf[msg.sender] -= _value;                    // Subtract from the sender
+        balanceOf[_to] += _value;                           // Add the same to the recipient
+        Transfer(msg.sender, _to, _value);
+    }
+}
